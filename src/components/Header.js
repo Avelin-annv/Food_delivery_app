767deb8842dd1c0252ec99968df6f1de9f6531ab
Avelin Ann <avelinannvarghese@gmail.com>
2024-03-21 16:36:39 +0530

    Changes for Home page

---
+++ src/components/Header.js
@@ -0,0 +1,56 @@
+import { useEffect, useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
+import { userContext } from "../userContext";
+import { useSelector, useDispatch } from "react-redux";
+const Header = () => {
+  const cartItems = useSelector((store) => store.cart.items);
+  const [btnName, setBtnName] = useState("Login");
+  const isOnline = useOnlineStatus();
+  const { loggedInUser } = useContext(userContext);
+
+  return (
+    <div className="flex justify-between border bg-slate-50 shadow-2xl">
+      <img
+        className="w-20 rounded-2xl m-4"
+        alt=" App logo"
+        src="https://cdn.dribbble.com/users/630677/screenshots/3833541/media/201454f743f48c415a38c49419275692.jpg?resize=400x300&vertical=center"
+      ></img>
+      <ul className=" flex m-4 p-4">
+        <li className="m-2.5">
+          {" "}
+          <Link to={"/grocery"}>Grocery</Link>
+        </li>
+        <li className="m-2.5">{`online status: ${isOnline ? "✅" : "❌"}`}</li>
+        <li className="m-2.5">
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li className="m-2.5">
+          <Link to={"/about"}>About</Link>
+        </li>
+        <li className="m-2.5">
+          {" "}
+          <Link to={"/contact"}>Contact us</Link>
+        </li>
+        {/* <li className="m-2.5">User:{loggedInUser}</li> */}
+        <li className="m-2.5">
+          <Link to="/cart">
+            🛒<label>{`Cart (${cartItems.length})`}</label>
+          </Link>
+        </li>
+        <li className="m-2.5">
+          {" "}
+          <button
+            className="login-btn"
+            onClick={() => {
+              setBtnName(btnName === "Login" ? "Logout" : "Login");
+            }}
+          >
+            {btnName}
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
+};
+export default Header;
---
+++ src/components/Home.js
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+
+import { swiggy_api_URL, SWIGGY_API_URL } from "../utils/constants";
+import useOnlineStatus from "../utils/useOnlineStatus";
+import RestaurantCardContainer from "./RestaurantCardContainer";
+import ShimmerComponent from "./ShimmerComponent";
+const Home = () => {
+  const [restaurantList, setRestaurantList] = useState([]);
+  const [searchList, setSearchList] = useState([]);
+  const [searchVal, setSearchVal] = useState("");
+  const isOnline = useOnlineStatus();
+  async function getData() {
+    //4 1
+    const res = await fetch(swiggy_api_URL);
+    const data = await res.json();
+
+    setRestaurantList(
+      data?.data.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+
+    setSearchList(
+      data?.data.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  }
+  const handleSearch = () => {
+    const filteredList = restaurantList?.filter((item) =>
+      item.info.name.toLowerCase().includes(searchVal.toLowerCase())
+    );
+    setSearchList(filteredList);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  if (!isOnline)
+    return (
+      <div>
+        <h1>Ooops..Looks like your internet connection is lost!!</h1>
+      </div>
+    );
+  return (
+    <div className="page-container p-[5%]">
+      <div className="my-5 mr-2.5 ml-0 flex align-middle">
+        <input
+          className="search-input m-3 w-[80%] border-2 border-black-1000 rounded-lg h-14"
+          value={searchVal}
+          placeholder="Search for restaurants and foods"
+          onChange={(e) => {
+            if (e.target.value === "") setSearchList(restaurantList);
+            setSearchVal(e.target.value);
+          }}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") handleSearch();
+          }}
+        ></input>
+        <button
+          className=" py-0 px-4 mt-5 h-10 rounded-lg bg-blue-400"
+          onClick={() => handleSearch()}
+        >
+          Search
+        </button>
+      </div>
+      {searchList?.length === 0 ? (
+        searchVal !== "" ? (
+          <div className="no-search">
+            <h2>{`No match found for "${searchVal}"`}</h2>
+          </div>
+        ) : (
+          <ShimmerComponent />
+        )
+      ) : (
+        <RestaurantCardContainer restaurantList={searchList} />
+      )}
+    </div>
+  );
+};
+
+export default Home;
